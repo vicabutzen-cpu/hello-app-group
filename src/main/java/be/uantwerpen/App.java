9ab39bb67635443d6e48b47753fc39d27aff3a44
vicab <vica.butzen@icloud.com>
2026-03-10 13:04:07 +0100

    Added wrong line

--- src/main/java/be/uantwerpen/App.java
+++ src/main/java/be/uantwerpen/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Dit is de feature branch.");
+        System.out.println("Foute wijziging");
     }
 }
