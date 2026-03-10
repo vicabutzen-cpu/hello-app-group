e94ea0138a5bd7750e31727e1f41fd0dc9c034c0
vicab <vica.butzen@icloud.com>
2026-03-10 12:47:58 +0100

    Added extra print on feature branch

--- src/main/java/be/uantwerpen/App.java
+++ src/main/java/be/uantwerpen/App.java
@@ -3,5 +3,6 @@ package be.uantwerpen;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Dit is de feature branch.");
     }
 }
